ad5e64ac762bed8deaf34ab7dc3f524d512836ca
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2017-01-30 18:31:57 +0100

    added some test-cases

--- src/ic/ImageComparison.java
+++ src/ic/ImageComparison.java
@@ -23,23 +23,80 @@ public class ImageComparison {
 	public static String imagePath3 = "data/P1080536.JPG";
 	public static String imagePath4 = "data/P1080538.JPG";
 
+	// public static String imagePath5 = "data/P1080565.JPG";
+	// public static String imagePath6 = "data/P1080566.JPG";
+
+	// public static String imagePath5 = "data/P1080578.JPG";
+	// public static String imagePath6 = "data/P1080579.JPG";
+
+	// public static String imagePath5 = "data/P1080581.JPG"; 4
+	// public static String imagePath6 = "data/P1080582.JPG";
+
+	// public static String imagePath5 = "data/P1080587.JPG"; 14
+	// public static String imagePath6 = "data/P1080588.JPG";
+
+	// public static String imagePath5 = "data/P1080589.JPG";
+	// public static String imagePath6 = "data/P1080590.JPG";
+
+//	public static String imagePath5 = "data/P1080596.JPG"; 11 
+//	public static String imagePath6 = "data/P1080597.JPG";
+	
+//	public static String imagePath5 = "data/P1080598.JPG"; 13
+//	public static String imagePath6 = "data/P1080599.JPG";
+
+	public static String imagePath5 = "data/P1080724.JPG"; 
+	public static String imagePath6 = "data/P1080733.JPG";
+	
+	
 	public static String alysonPath = "data/Alyson_Hannigan_200512.jpg";
 
 	public static void main(String[] args) throws IOException {
-//		Log.setDebugMode();
+		// Log.setDebugMode();
 		// Log.setQuiet();
 		// rotate();
-		dHash();
+		// dHash();
+
+		dHash2();
 //		Runtimes.printReport();
-		
-//		dHash2();
 	}
-	
-	
+
 	public static void dHash2() throws IOException {
-		BufferedImage image1 = IO.readImage(imagePath1);
-		BufferedImage image2 = IO.readImage(imagePath2);
-		
+//		BufferedImage image1 = IO.readImage(imagePath1);
+//		BufferedImage image2 = IO.readImage(imagePath2);
+//		BufferedImage image3 = IO.readImage(imagePath3);
+//		BufferedImage image4 = IO.readImage(imagePath4);
+		BufferedImage image5 = IO.readImage(imagePath5);
+		BufferedImage image6 = IO.readImage(imagePath6);
+//
+//		DHash hash1 = DHash.getDHash(image1);
+//		DHash hash2 = DHash.getDHash(image2);
+//		DHash hash3 = DHash.getDHash(image3);
+//		DHash hash4 = DHash.getDHash(image4);
+		DHash hash5 = DHash.getDHash(image5);
+		DHash hash6 = DHash.getDHash(image6);
+
+//		Log.log("1 vs all");
+//		System.out.println(hash1.compareTo(hash2));
+//		System.out.println(hash1.compareTo(hash3));
+//		System.out.println(hash1.compareTo(hash4));
+//
+//		Log.log("2 vs all");
+//		System.out.println(hash2.compareTo(hash1));
+//		System.out.println(hash2.compareTo(hash3));
+//		System.out.println(hash2.compareTo(hash4));
+//
+//		Log.log("3 vs all");
+//		System.out.println(hash3.compareTo(hash1));
+//		System.out.println(hash3.compareTo(hash2));
+//		System.out.println(hash3.compareTo(hash4));
+//
+//		Log.log("4 vs all");
+//		System.out.println(hash4.compareTo(hash1));
+//		System.out.println(hash4.compareTo(hash2));
+//		System.out.println(hash4.compareTo(hash3));
+
+		Log.log("5 vs 6");
+		System.out.println(hash5.compareTo(hash6));
 	}
 
 	public static void dHash() throws IOException {
@@ -51,13 +108,13 @@ public class ImageComparison {
 		DHash hash2 = DHash.getDHash(i2);
 
 		BufferedImage i3 = Processing.resize(i, 1.5, 1.0);
-		DHash  hash3 = DHash.getDHash(i3);
+		DHash hash3 = DHash.getDHash(i3);
 
 		BufferedImage i4 = Processing.grayScale(i);
-		DHash  hash4 = DHash.getDHash(i4);
+		DHash hash4 = DHash.getDHash(i4);
 
 		BufferedImage i5 = Processing.rotate(i, 90);
-		DHash  hash5 = DHash.getDHash(i5);
+		DHash hash5 = DHash.getDHash(i5);
 
 		System.out.println(hash.compareTo(hash2));
 		System.out.println(hash.compareTo(hash3));
