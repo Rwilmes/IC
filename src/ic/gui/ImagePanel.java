0027de62350b71057257b4cdf9fff6b521cc024f
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2017-02-04 16:14:45 +0100

    added ImagePanel

---
+++ src/ic/gui/ImagePanel.java
@@ -0,0 +1,34 @@
+package ic.gui;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+
+/**
+ * Extends the basic JPanel and overwrites the paint method to enable painting
+ * of images.
+ * 
+ * @author Rwilmes
+ * 
+ */
+public class ImagePanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private final BufferedImage img;
+
+	public ImagePanel(BufferedImage img) {
+		this.img = img;
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(img, 0, 0, null);
+	}
+
+}
