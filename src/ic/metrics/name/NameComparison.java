2f75c11e8d76a10949cdfa50a66130324ff24170
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2017-03-01 20:35:12 +0100

    minor rework on NameComparison

--- src/ic/metrics/name/NameComparison.java
+++ src/ic/metrics/name/NameComparison.java
@@ -5,15 +5,14 @@ import ic.image.Image;
 /** A simple metric comparing only the filenames of two images. **/
 public class NameComparison {
 
-	private String name;
-
-	public NameComparison(String name) {
-		this.name = name;
-	}
-
 	/** Compares two images. Returns 0 if they are equal, 1 else. **/
 	public static int compare(Image i1, Image i2) {
-		if (i1.getFilename().equals(i2.getFilename()))
+		return NameComparison.compare(i1.getFilename(), i2.getFilename());
+	}
+
+	/** Compares two filenames. Returns 0 if they are equal, 1 else. **/
+	public static int compare(String filename1, String filename2) {
+		if (filename1.equals(filename2))
 			return 0;
 		else
 			return 1;
