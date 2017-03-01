918174b011cef68c37c831639dc5f9e57563999c
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2017-03-01 22:17:26 +0100

    added ImageComparator and ImageComparisonPolicy

---
+++ src/ic/ImageComparator.java
@@ -0,0 +1,30 @@
+package ic;
+
+import ic.image.Image;
+import ic.metrics.hashes.ImageHash;
+import ic.util.Config;
+
+/**
+ * The ImageComparator compares two images based on a given
+ * ImageComparisonPolicy and returns if their distance is sufficiently small.
+ * 
+ * @author Rwilmes
+ * 
+ */
+public class ImageComparator {
+
+	private ImageComparisonPolicy policy;
+
+	public ImageComparator(ImageComparisonPolicy policy) {
+		this.policy = policy;
+	}
+	
+	public boolean compare(Image i1, Image i2) {
+		for (Class<? extends ImageHash> c : Config.getHashClassesToCompute()) {
+			int distance = i1.getHash(c).compareTo(i2.getHash(c));
+			
+		}
+		
+		return true;
+	}
+}
---
+++ src/ic/ImageComparisonPolicy.java
@@ -0,0 +1,26 @@
+package ic;
+
+import ic.image.Image;
+import ic.metrics.hashes.ImageHash;
+import ic.util.Config;
+
+/**
+ * An ImageComparisonPolicy defines which characteristics have to be met for two
+ * images to be categorized as being the same image.
+ * 
+ * @author Rwilmes
+ * 
+ */
+public class ImageComparisonPolicy {
+
+	public ImageComparisonPolicy() {
+
+	}
+
+	/** Returns true if both images are identified as the same image. **/
+	public boolean are(Image i1, Image i2) {
+
+
+		return true;
+	}
+}
